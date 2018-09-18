aca62c92f7e2e7086cb704cc802f083153d1b66a
Federico Meini <meini@mpib-berlin.mpg.de>
2018-09-18 11:47:55 +0200

    v1.3.0

--- src/providers/stimuli/app-info.ts
+++ src/providers/stimuli/app-info.ts
@@ -1,5 +1,5 @@
 export abstract class AppInfo {
     static id: string = "fe44c716";
     static nameLabel: string = "Grids";
-    static version: string = "1.2.0";
+    static version: string = "1.3.0";
 }
