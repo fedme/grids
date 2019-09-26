c5e2a3c41c5e6dc3b10fbcae9908f532a1988edd
costanza <desimone@mpib-berlin.mpg.de>
2019-09-26 18:11:52 +0200

    Updated version nr.

--- src/providers/stimuli/app-info.ts
+++ src/providers/stimuli/app-info.ts
@@ -1,5 +1,5 @@
 export abstract class AppInfo {
     static id: string = "fe44c716";
     static nameLabel: string = "Grids";
-    static version: string = "1.6.0";
+    static version: string = "1.6.1";
 }
